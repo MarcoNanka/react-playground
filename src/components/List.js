a3b2f5eb73e20810dde4d500a03b87dd1444165e
MarcoNanka <marco.nanka@tum.de>
2022-03-20 18:54:43 +0100

    Added functionality of TODO list

--- src/components/List.js
+++ src/components/List.js
@@ -1,29 +1,55 @@
 import {useState} from 'react';
 
 const List = () => {
-
-   const tasks = [
-      {title: 'putzen', completed: false, key: 0},
-      {title: 'kochen', completed: false, key: 1},
-      {title: 'backen', completed: false, key: 2}
+   const initialTasks = [
+      {title: 'putzen', completed: false},
+      {title: 'kochen', completed: false},
+      {title: 'backen', completed: false},
    ];
 
+   const [tasks, setTasks] = useState(
+      initialTasks
+   );
+
+   const [input, setInput] = useState(
+      ''
+   );
+
+   const deleteTask = (index) => {
+      const newTasks = [...tasks];
+      newTasks.splice(index, 1); //löscht eintrag bei newTasks[index]
+      setTasks(newTasks);
+   };
+
+   const completeTask = (index) => {
+      const newTasks = [...tasks];
+      newTasks[index].completed = true;
+      setTasks(newTasks);
+   };
+
+   const addTask = (title) => {
+      setTasks([...tasks, {title}])
+   };
    return (
       <div className="list">
          List
          <ul>
-            {tasks.map((task)=>(
-            <li>
+            {tasks.map((task, index)=>(
+            <li key={index} className = {task.completed ? 'completed' : null}>
                   <div>
                   <p>{task.title}</p>
-                  <button>complete</button>
-                  <button>delete</button>
+                  <button onClick={() => completeTask(index)}>complete</button>
+                  <button onClick={() => deleteTask(index)}>delete</button>
                </div>
             </li>
                ))}
          </ul>
-         <form>
-            <input type="text" />
+         <form onSubmit={(e) => {
+            e.preventDefault();
+            addTask(input);
+            setInput('');
+            }}>
+            <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
             <button>Plus</button>
          </form>
       </div>
